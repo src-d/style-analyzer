425b66dcb4bc6feffa53321ab801847befbe933d
Vadim Markovtsev <vadim@sourced.tech>
2018-07-25 18:32:30 +0200

    Fix the grpc imports

--- lookout/core/generate_rpc.sh
+++ lookout/core/generate_rpc.sh
@@ -8,3 +8,5 @@ python3 -m grpc_tools.protoc -I$base_dir/server/sdk \
     --python_out=$out_dir --grpc_python_out=$out_dir \
     $base_dir/server/sdk/*.proto
 touch $out_dir/__init__.py
+# https://github.com/google/protobuf/issues/1491
+find -name '*_pb2_grpc.py' -exec sed -Ei 's/(import [^ ]+_pb2 as)/from . \1/g' {} \;
