f6a302e7a747be3cc6cb86e28ca346945c72884f
Vadim Markovtsev <vadim@sourced.tech>
2018-07-28 00:40:39 +0200

    Add the dummy FormatAnalyzer
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- lookout/core/analyzer.py
+++ lookout/core/analyzer.py
@@ -1,18 +1,24 @@
-from typing import Type
+from typing import Type  # noqa: F401
 
 from modelforge import Model
 
+from lookout.core.api.service_analyzer_pb2 import Comment
+from lookout.core.api.service_data_pb2_grpc import DataStub
+
 
 class Analyzer:
-    version: str = None
-    model_type: Type[Model] = None
+    version = None  # type: str
+    model_type = None  # type: Type[Model]
 
     def __init__(self, model: Model, url: str, config: dict):
-        raise NotImplementedError
+        self.model = model
+        self.url = url
+        self.config = config
 
-    def analyze(self, commit_from: str, commit_to: str):
+    def analyze(self, commit_from: str, commit_to: str, data_request_stub: DataStub,
+                **data) -> [Comment]:
         raise NotImplementedError
 
     @classmethod
-    def train(cls, url: str, commit: str, config: str):
+    def train(cls, url: str, commit: str, config: dict, data_request_stub: DataStub, **data):
         raise NotImplementedError
--- lookout/core/api/gopkg/in/bblfsh/sdk/v1/uast/generated_pb2.py
+++ lookout/core/api/gopkg/in/bblfsh/sdk/v1/uast/generated_pb2.py
@@ -14,7 +14,7 @@ from google.protobuf import descriptor_pb2
 _sym_db = _symbol_database.Default()
 
 
-from .github.com.gogo.protobuf.gogoproto import gogo_pb2 as github_dot_com_dot_gogo_dot_protobuf_dot_gogoproto_dot_gogo__pb2
+from lookout.core.api.github.com.gogo.protobuf.gogoproto import gogo_pb2 as github_dot_com_dot_gogo_dot_protobuf_dot_gogoproto_dot_gogo__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
