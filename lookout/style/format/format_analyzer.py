6f72ee9f908b222eed0a988bd9019f86ade399b7
Vadim Markovtsev <vadim@sourced.tech>
2018-08-08 16:48:00 +0200

    Fix the compatibility with bblfsh
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- lookout/style/format/format_analyzer.py
+++ lookout/style/format/format_analyzer.py
@@ -7,7 +7,7 @@ from lookout.core.analyzer import Analyzer
 from lookout.core.analyzer_model import AnalyzerModel
 from lookout.core.api.service_analyzer_pb2 import Comment
 from lookout.core.api.service_data_pb2_grpc import DataStub
-Node = importlib.import_module("lookout.core.api.gopkg.in.bblfsh.sdk.v1.uast.generated_pb2").Node
+Node = importlib.import_module("bblfsh.gopkg.in.bblfsh.sdk.v1.uast.generated_pb2").Node
 from lookout.core.data_requests import with_changed_uasts, with_uasts  # noqa: E401
 
 
