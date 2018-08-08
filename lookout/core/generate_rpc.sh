1c890657b12ec508109655627100558187d8556c
Vadim Markovtsev <vadim@sourced.tech>
2018-08-08 16:48:00 +0200

    Fix the compatibility with bblfsh
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- lookout/core/generate_rpc.sh
+++ lookout/core/generate_rpc.sh
@@ -1,21 +1,19 @@
 #!/bin/sh -ex
 
 base_dir=$(dirname "$(readlink -f \"$0\")")
-out_dir=$base_dir/api
+out_dir=$base_dir/../..
+real_out_dir=$base_dir/api
+tmp_dir=$base_dir/lookout/core/api
 
-mkdir -p $out_dir
-python3 -m grpc_tools.protoc -I$base_dir/server/sdk \
+rm -rf $real_out_dir
+rm -rf $tmp_dir
+mkdir -p $tmp_dir
+cp -r $base_dir/server/sdk/* $tmp_dir
+find $tmp_dir -name '*.proto' -exec sed -i '/"google/! {/\//! s/import "/import "lookout\/core\/api\//g}' {} \;
+python3 -m grpc_tools.protoc -I$base_dir -I$tmp_dir \
     --python_out=$out_dir --grpc_python_out=$out_dir \
-    $base_dir/server/sdk/*.proto
-python3 -m grpc_tools.protoc -I$base_dir/server/sdk \
-    --python_out=$out_dir \
-    $base_dir/server/sdk/github.com/gogo/protobuf/gogoproto/gogo.proto
-python3 -m grpc_tools.protoc -I$base_dir/server/sdk \
-    --python_out=$out_dir \
-    $base_dir/server/sdk/gopkg.in/bblfsh/sdk.v1/uast/generated.proto
-touch $out_dir/__init__.py
-# https://github.com/google/protobuf/issues/1491
-find $out_dir -name '*.py' -exec sed -Ei 's/^(import [^ ]+_pb2 as)/from . \1/g' {} \;
-find $out_dir -name '*.py' -exec sed -i 's/from github/from lookout.core.api.github/g' {} \;
-find $out_dir -name '*.py' -exec sed -i 's/from gopkg/from lookout.core.api.gopkg/g' {} \;
-find $out_dir -name '*.py' -exec sed -i "s/importlib.import_module('/importlib.import_module('lookout.core.api./g" {} \;
+    $tmp_dir/*.proto
+find $real_out_dir -name '*.py' -exec sed -i 's/^from github/from bblfsh.github/g' {} \;
+find $real_out_dir -name '*.py' -exec sed -i "s/import_module('gopkg.in/import_module('bblfsh.gopkg.in/g" {} \;
+touch $real_out_dir/__init__.py
+rm -r $base_dir/lookout
