d6bcc9bb5cd42c67c2805d9537f8fdca7a93fc8b
Vadim Markovtsev <vadim@sourced.tech>
2018-08-14 19:04:44 +0200

    Add FeatureExtractor._classify_vnodes()
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- lookout/style/format/tests/test_features.py
+++ lookout/style/format/tests/test_features.py
@@ -1,27 +1,61 @@
 import lzma
+from collections import defaultdict
 from pathlib import Path
 import unittest
 
 import bblfsh
 
-from lookout.style.format.features import parse_file
+from lookout.style.format.features import FeatureExtractor, CLASSES, CLS_SPACE_INC, \
+    CLS_SPACE_DEC, CLS_NEWLINE, CLS_SPACE, CLS_SINGLE_QUOTE
 
 
 class FeaturesTests(unittest.TestCase):
-    def test_parse_file(self):
+    @classmethod
+    def setUpClass(cls):
         base = Path(__file__).parent
         # str() is needed for Python 3.5
         with lzma.open(str(base / "benchmark.js.xz"), mode="rt") as fin:
-            contents = fin.read()
+            cls.contents = fin.read()
         with lzma.open(str(base / "benchmark.uast.xz")) as fin:
-            uast = bblfsh.Node.FromString(fin.read())
-        nodes, parents = parse_file(contents, uast, "javascript")
+            cls.uast = bblfsh.Node.FromString(fin.read())
+        cls.extractor = FeatureExtractor("javascript")
+
+    def test_parse_file(self):
+        nodes, parents = self.extractor._parse_file(self.contents, self.uast)
         text = []
+        offset = line = col = 0
         for n in nodes:
+            if line == n.start.line - 1:
+                line += 1
+                col = 1
+            self.assertEqual((offset, line, col), n.start, n.value)
             text.append(n.value)
             if n.node is not None:
                 self.assertIsNotNone(parents.get(id(n.node)), n)
-        self.assertEqual("".join(text), contents)
+            offset, line, col = n.end
+        self.assertEqual("".join(text), self.contents)
+
+    def test_classify_vnodes(self):
+        nodes, _ = self.extractor._parse_file(self.contents, self.uast)
+        nodes = self.extractor._classify_vnodes(nodes)
+        text = "".join(n.value for n in nodes)
+        self.assertEqual(text, self.contents)
+        cls_counts = defaultdict(int)
+        offset = line = col = 0
+        for n in nodes:
+            if line == n.start.line - 1:
+                line += 1
+                col = 1
+            self.assertEqual((offset, line, col), n.start, n.value)
+            if n.y is not None:
+                cls_counts[CLASSES[n.y]] += 1
+            offset, line, col = n.end
+        self.assertEqual(cls_counts[CLS_SPACE_INC], cls_counts[CLS_SPACE_DEC])
+        self.assertGreater(cls_counts[CLS_SPACE_INC], 0)
+        self.assertGreater(cls_counts[CLS_SPACE], 0)
+        self.assertGreater(cls_counts[CLS_NEWLINE], 0)
+        self.assertGreater(cls_counts[CLS_SINGLE_QUOTE], 0)
+        self.assertTrue(cls_counts[CLS_SINGLE_QUOTE] % 2 == 0)
 
 
 if __name__ == "__main__":
