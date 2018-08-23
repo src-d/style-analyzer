6fe0f68471d0b2adf39132b2ba1710f3e86ab9d9
Vadim Markovtsev <vadim@sourced.tech>
2018-08-23 15:59:27 +0200

    Add slogging test
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

---
+++ lookout/core/tests/test_slogging.py
@@ -0,0 +1,31 @@
+import io
+import json
+import logging
+import sys
+import unittest
+
+from lookout.core import slogging
+
+
+class SloggingTests(unittest.TestCase):
+    def test_structured_logging(self):
+        logging.basicConfig()
+        handler_backup = logging.getLogger().handlers[0]
+        slogging.setup("INFO", True)
+        backup = sys.stdout
+        sys.stdout = buffer = io.StringIO()
+        try:
+            logging.getLogger("test").info("hello, world!")
+        finally:
+            sys.stdout = backup
+        logging.getLogger().handlers[0] = handler_backup
+        obj = json.loads(buffer.getvalue())
+        self.assertEqual(obj["level"], "info")
+        self.assertEqual(obj["msg"], "hello, world!")
+        self.assertEqual(obj["source"], "test_slogging.py:18")
+        self.assertEqual(len(obj["thread"]), 4)
+        self.assertIn("time", obj)
+
+
+if __name__ == "__main__":
+    unittest.main()
