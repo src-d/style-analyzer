8c9bc465fcdea72f57410a211be063d902b83088
Vadim Markovtsev <vadim@sourced.tech>
2018-09-10 16:50:21 +0200

    Change the i-test to not launch Python processes
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- lookout/style/format/tests/test_analyzer_integration.py
+++ lookout/style/format/tests/test_analyzer_integration.py
@@ -1,48 +1,35 @@
+import logging
+import os
 from pathlib import Path
-import queue
 import re
 import shutil
-import signal
-import subprocess
-import sys
 import tarfile
 import tempfile
+import threading
 from threading import Thread
 import unittest
 from unittest.mock import patch
 
-from lookout.__main__ import main as analyzer
-from lookout.core.tests.server import find_port, run
+from lookout.__main__ import main as launch_analyzer
+from lookout.core.event_listener import EventListener
+from lookout.core.tests.server import find_port, run as launch_server
 
 
+@unittest.skipUnless(os.getenv("LONG_TESTS", False),
+                     "Time-consuming tests are skipped by default.")
 class AnalyzerIntegrationTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        base_path = Path(__file__).parent.resolve()
-        cls.base_dir = str(base_path)
-        print(cls.base_dir)
-        print(cls.base_dir)
-        cls.jquery_dir = str(base_path / "jquery")
+        parent = Path(__file__).parent.resolve()
+        cls.base_dir = str(parent)
+        cls.jquery_dir = str(parent / "jquery")
         # str() is needed for Python 3.5
-        with tarfile.open(str(base_path / "jquery.tar.xz")) as tar:
-            tar.extractall(path=str(cls.base_dir))
+        with tarfile.open(str(parent / "jquery.tar.xz")) as tar:
+            tar.extractall(path=cls.base_dir)
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.jquery_dir)
-
-    def _wait_ready(self):
-        for line in iter(self.analyzer.stdout.readline, b""):
-            line = line.decode("utf-8", "replace")
-            if line.find(":run:Listening localhost:%d" % self.port) != -1:
-                return True
-
-    def _tee(self):
-        for line in iter(self.analyzer.stdout.readline, b""):
-            line = line.decode("utf-8", "replace")
-            line = line.rstrip()
-            print(line, file=sys.stderr)
-            self.analyzer_output.put(line)
 
     def setUp(self):
         self.port = find_port()
@@ -50,43 +37,55 @@ class AnalyzerIntegrationTests(unittest.TestCase):
         self.fs = tempfile.TemporaryDirectory(dir=self.base_dir)
         command = "analyzer init --db sqlite:///%s --fs %s" % (self.db.name, self.fs.name)
         with patch("sys.argv", command.split(" ")):
-            analyzer()
-        command = ("python3 -m lookout run lookout.style.format --db sqlite:///%s "
+            launch_analyzer()
+        command = ("analyzer run lookout.style.format --db sqlite:///%s "
                    "--server localhost:%d --fs %s --log-level DEBUG"
                    ) % (self.db.name, self.port, self.fs.name)
-        self.analyzer = subprocess.Popen(
-            command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        self.logs = logs = []
 
-        waiter = Thread(target=self._wait_ready)
-        waiter.start()
-        waiter.join(5)
-        if waiter.is_alive():
-            raise RuntimeError("couldn't start the analyzer correctly")
-        self.analyzer_output = queue.Queue()
-        self.tee = Thread(target=self._tee)
-        self.tee.start()
+        class ShadowHandler(logging.Handler):
+            def emit(self, record):
+                logs.append(logging.getLogger().handlers[0].formatter.format(record))
+
+        self.log_handler = ShadowHandler()
+        logging.getLogger().addHandler(self.log_handler)
+
+        def main():
+            with patch("sys.argv", command.split(" ")):
+                launch_analyzer()
+
+        sync = threading.Event()
+        block = EventListener.block
+
+        def sync_block(this):
+            self.listener = this
+            sync.set()
+            return block(this)
+
+        EventListener.block = sync_block
+        self.process = Thread(target=main)
+        try:
+            self.process.start()
+            sync.wait()
+        finally:
+            EventListener.block = block
 
     def tearDown(self):
-        self.analyzer.send_signal(signal.SIGINT)
-        self.analyzer.wait()
-        self.analyzer.stdout.close()
-        self.tee.join()
+        logging.getLogger().removeHandler(self.log_handler)
+        self.listener.stop()
+        self.process.join()
         self.db.close()
         self.fs.cleanup()
 
     def test_review(self):
-        run("review", "fbd32214d1b08e09278f39c77979658cabde6c4d",
-            "931442679ad04ebadad2b70a9f938fb0bc64d537", self.port, git_dir=self.jquery_dir)
-        while True:
-            try:
-                line = self.analyzer_output.get_nowait()
-            except queue.Empty:
-                break
-            matches = re.search(r"FormatAnalyzer: (\d+) comments", line)
-            if matches:
-                self.assertGreater(int(matches.group(1)), 0)
-                return
-        self.fail("did not output any comment")
+        launch_server(
+            "review",
+            "fbd32214d1b08e09278f39c77979658cabde6c4d",
+            "931442679ad04ebadad2b70a9f938fb0bc64d537",
+            self.port, git_dir=self.jquery_dir)
+        matches = re.search(r"FormatAnalyzer: (\d+) comments", "".join(self.logs))
+        self.assertTrue(matches)
+        self.assertGreater(int(matches.group(1)), 0)
 
 
 if __name__ == "__main__":
