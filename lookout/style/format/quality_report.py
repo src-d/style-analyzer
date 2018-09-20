826f549bc3f7f782f8975eee6c830498521dfdbf
Hugo Mougard <hugo@sourced.tech>
2018-09-20 09:37:40 +0200

    Transform debugging scripts so that they can be used as libs easily
    Signed-off-by: Hugo Mougard <hugo@sourced.tech>

--- lookout/style/format/quality_report.py
+++ lookout/style/format/quality_report.py
@@ -2,7 +2,7 @@ from collections import Counter
 import glob
 import os
 
-import bblfsh
+from bblfsh import BblfshClient
 from bblfsh.client import NonUTF8ContentException
 import numpy
 from sklearn.metrics import classification_report,confusion_matrix
@@ -36,16 +36,17 @@ def prepare_files(folder, client, language):
     return files
 
 
-def quality_report(args):
-    client = bblfsh.BblfshClient(args.bblfsh)
-    files = prepare_files(args.input, client, args.language)
+def quality_report(input_pattern: str, bblfsh: str, language: str, n_files: int,
+                   model: str) -> None:
+    client = BblfshClient(bblfsh)
+    files = prepare_files(input_pattern, client, language)
     print("Number of files: %s" % (len(files)))
 
-    fe = FeatureExtractor(language=args.language)
+    fe = FeatureExtractor(language=language)
     X, y, nodes = fe.extract_features(files)
 
-    analyzer = FormatModel().load(args.model)
-    rules = analyzer._rules_by_lang[args.language]
+    analyzer = FormatModel().load(model)
+    rules = analyzer._rules_by_lang[language]
     y_pred = rules.predict(X)
 
     target_names = [CLASSES[cls_ind] for cls_ind in numpy.unique(y)]
@@ -60,7 +61,7 @@ def quality_report(args):
     file_stat = Counter(file_mispred)
 
     to_show = file_stat.most_common()
-    if args.n_files > 0:
-        to_show = to_show[:args.n_files]
+    if n_files > 0:
+        to_show = to_show[:n_files]
 
     print("Files with most errors:\n" + "\n".join(map(str, to_show)))
