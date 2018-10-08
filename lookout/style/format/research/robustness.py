2fd9acc22907f267d73fe398b452be2feaa1b773
Waren Long <waren@sourced.tech>
2018-10-08 17:06:42 +0200

    Extract list of style-analyzer fixes in the noisy dataset 1-mistake-per-file
    Signed-off-by: Waren Long <waren@sourced.tech>

--- lookout/style/format/research/robustness.py
+++ lookout/style/format/research/robustness.py
@@ -3,43 +3,39 @@ from collections import defaultdict, namedtuple
 from difflib import SequenceMatcher
 import glob
 import os
+from typing import Callable, Dict, List, Set, Tuple
 
 from bblfsh import BblfshClient
+import numpy
 
 from lookout.core.slogging import setup
 from lookout.style.format.feature_extractor import FeatureExtractor
+from lookout.style.format.feature_utils import CLASS_INDEX
 from lookout.style.format.files_filtering import filter_filepaths
 from lookout.style.format.model import FormatModel
 from lookout.style.format.quality_report import prepare_files
-from lookout.style.format.feature_utils import CLASS_INDEX
 
 
 Misprediction = namedtuple("Misprediction", ["y", "pred", "node", "rule"])
 
 
-def get_content_from_repo(folder: str):
+def get_content_from_repo(folder: str) -> Dict[str, str]:
     content = {}
     filenames = glob.glob(folder, recursive=True)
     for file in filter_filepaths(filenames):
         if file.endswith(".js"):
             with open(file, "r") as g:
-                #"/".join(file.split("/")[file.split("/").index("jquery"):])
                 content[file] = g.read()
-                #content.append((file, g.read()))
     return content
 
     
-def get_difflib_changes(true_content, noisy_content):
+def get_difflib_changes(true_content: Dict[str, str], noisy_content: Dict[str, str]
+                        ) -> Tuple[List[str], List[str], Dict[str, Set[int]], int]:
     true_files, noisy_files = set(), set()
     nb_changes = 0
     lines_changed = defaultdict(set)
-    #for i in range(len(true_content)):
-    #for i in range(len(noisy_content)):
     for (tf, tc), (nf, nc) in zip(true_content.items(), noisy_content.items()):
-        #import pdb;pdb.set_trace()
-        #for file in noisy_content.keys():
         matcher = SequenceMatcher(a=tc.splitlines(), b=nc.splitlines())
-        #matcher = SequenceMatcher(a=true_content[i][1].splitlines(), b=noisy_content[i][1].splitlines())
         for action, _, _, j1, j2 in matcher.get_opcodes():
             if action is not "equal":
                 lines_changed[nf].update(range(j1, j2+1))
@@ -49,7 +45,8 @@ def get_difflib_changes(true_content, noisy_content):
     return sorted(list(true_files)), sorted(list(noisy_files)), lines_changed, nb_changes
 
 
-def get_mispreds(y, y_pred, nodes, winner):
+def get_mispreds(y: numpy.ndarray, y_pred: numpy.ndarray, nodes: List[Callable],
+                 winner: numpy.ndarray) -> List[Callable]:
     mispreds = []
     for gt, pred, vn, rule in zip(y, y_pred, nodes, winner):
         if gt != pred:
@@ -57,7 +54,8 @@ def get_mispreds(y, y_pred, nodes, winner):
     return mispreds
 
 
-def get_diff_mispreds(mispreds_noise, lines_changed):
+def get_diff_mispreds(mispreds_noise: List[Callable], lines_changed: Dict[str, Set[int]]
+                      ) -> Dict[str, Callable]:
     diff_mispreds = {}
     for m in mispreds_noise:
         mispred_lines = set(range(m.node.start.line, m.node.end.line+1))
@@ -70,7 +68,9 @@ def get_diff_mispreds(mispreds_noise, lines_changed):
     return diff_mispreds
 
 
-def files2mispreds(files, rules, client, language):
+def files2mispreds(files: List[str], rules: Callable, client: str, language: str
+                   ) -> Tuple[List[Callable], List[Callable]]:
+    import pdb;pdb.set_trace()
     files = prepare_files(files, client, language)
     fe = FeatureExtractor(language=language, **rules.origin_config["feature_extractor"])
     X, y, vnodes_y, _ = fe.extract_features(files)
@@ -80,37 +80,50 @@ def files2mispreds(files, rules, client, language):
     return mispreds, vnodes_y
 
 
-def style_robustness_report(true_repo: str, noisy_repo: str, bblfsh: str, language: str, model: str):
-    client = BblfshClient(bblfsh)
-
-    true_content = get_content_from_repo(true_repo)
-    noisy_content = get_content_from_repo(noisy_repo)
-
-    true_files, noisy_files, lines_changed, nb_changes = get_difflib_changes(true_content, noisy_content)
-    print("Number of files modified by adding style noise : %d / %d" % (len(true_files), len(true_content)))
-    #del true_content, noisy_content
-
-    analyzer = FormatModel().load(model)
-    rules = analyzer[language]
-
-    mispreds_true, vnodes_y_true = files2mispreds(true_files, rules, client, language)
-    mispreds_noise, _ = files2mispreds(noisy_files, rules, client, language)
-
-    diff_mispreds = get_diff_mispreds(mispreds_noise, lines_changed)
-    print("Number of artificial mistakes potentially fixed by the model : %d / %d" % (len(diff_mispreds), nb_changes))
-    
+def get_style_fixes(diff_mispreds: Dict[str, Callable], vnodes: List[Callable],
+                    true_files: List[str], noisy_files: List[str]) -> List[Callable]:
+    import pdb;pdb.set_trace()
     style_fixes = []
     for i in range(len(true_files)):
         try:
             mispred = diff_mispreds[noisy_files[i]]
         except KeyError:
             continue
-        for vn in vnodes_y_true:
+        for vn in vnodes:
             if vn.path == true_files[i] and vn.start.offset == mispred.node.start.offset:
                 if mispred.pred == vn.y :
                     style_fixes.append(mispred)
                 break
-    print("style-analyzer fixes in the noisy repo : %d / %d" % (len(style_fixes), nb_changes))
+    return style_fixes
+
+
+def style_robustness_report(true_repo: str, noisy_repo: str, bblfsh: str, language: str,
+                            model: str) -> None:
+    client = BblfshClient(bblfsh)
+
+    true_content = get_content_from_repo(true_repo)
+    noisy_content = get_content_from_repo(noisy_repo)
+    true_files, noisy_files, lines_changed, nb_changes = get_difflib_changes(true_content, noisy_content)
+
+    print()
+    print("Number of files modified by adding style noise : %d / %d" % (len(true_files), len(true_content)))
+    del true_content, noisy_content
+    
+    analyzer = FormatModel().load(model)
+    rules = analyzer[language]
+
+    _, vnodes_y_true = files2mispreds(true_files, rules, client, language)
+    mispreds_noise, _ = files2mispreds(noisy_files, rules, client, language)
+
+    diff_mispreds = get_diff_mispreds(mispreds_noise, lines_changed)
+    print("Number of artificial mistakes potentially fixed by the model (diff of mispredictions): %d / %d"
+        % (len(diff_mispreds), nb_changes))
+    
+    style_fixes = get_style_fixes(diff_mispreds, vnodes_y_true, true_files, noisy_files)
+    
+    print("style-analyzer fixes in the noisy repos : %d / %d -> %.1f %%"
+        % (len(style_fixes), nb_changes, 100 *len(style_fixes) / nb_changes))
+    print()
     print("list of files where the style-analyzer succeeds in fixing the random noise :")
     for mispred in style_fixes:
         print(mispred.node.path)
