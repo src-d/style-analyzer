2d75d29a622759782e2f5c8198056de89ba8879d
Vadim Markovtsev <vadim@sourced.tech>
2018-10-10 14:58:54 +0200

    Fix Vadim's review

--- lookout/style/typos/research/__main__.py
+++ lookout/style/typos/research/__main__.py
@@ -1,8 +1,11 @@
 import argparse
+import logging
 import sys
 
+from lookout.core import slogging
 from lookout.core.cmdline import ArgumentDefaultsHelpFormatterNoNone
-from lookout.style.typos.nn_prediction import create_and_train_nn_prediction_from_file
+from lookout.style.typos.research.nn_prediction import create_and_train_nn_prediction_from_file, \
+    DEFAULT_NUM_NEURONS, DEFAULT_BATCH_SIZE, DEFAULT_LR, DEFAULT_DECAY, DEFAULT_NUM_EPOCHS
 from lookout.style.typos.utils import CORRECT_TOKEN_COLUMN, TYPO_COLUMN
 
 
@@ -10,22 +13,31 @@ def create_parser():
     parser = argparse.ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatterNoNone,
                                      description="Train nn model for correction embedding"
                                                  "prediction.")
-    parser.add_argument("fasttext_file", type=str,
-                        help="Path to binary dump of fasttext model.")
-    parser.add_argument("data_file", type=str,
-                        help="Path to csv dump of pandas.DataFrame containing"
+    parser.add_argument("--fasttext", type=str, required=True,
+                        help="Path to the binary dump of a FastText model.")
+    parser.add_argument("--data", type=str, required=True,
+                        help="Path to a CSV dump of pandas.DataFrame containing"
                              "columns [%s, %s]" % (CORRECT_TOKEN_COLUMN, TYPO_COLUMN))
-    parser.add_argument("dump", type=str, help="Path to file to dump trained nn model.")
-    parser.add_argument("--batch_size", type=int, default=64, help="Batch size for training.")
-    parser.add_argument("--lr", type=float, default=0.1, help="Learning rate.")
-    parser.add_argument("--decay", type=float, default=0.9, help="Decay.")
-    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs.")
+    parser.add_argument("--dump", type=str,
+                        help="Path to the file where to dump the trained NN model.")
+    parser.add_argument("--num-neurons", type=int, help="Number of neurons in each hidden layer.",
+                        default=DEFAULT_NUM_NEURONS)
+    parser.add_argument("--batch-size", type=int, default=DEFAULT_BATCH_SIZE,
+                        help="Batch size for training.")
+    parser.add_argument("--lr", type=float, default=DEFAULT_LR, help="Learning rate.")
+    parser.add_argument("--decay", type=float, default=DEFAULT_DECAY,
+                        help="Learning rate exponential decay per epoch.")
+    parser.add_argument("--num-epochs", type=int, default=DEFAULT_NUM_EPOCHS,
+                        help="Number of training passes over the dataset.")
+    parser.add_argument("--log-level", default="INFO", choices=logging._nameToLevel,
+                        help="Logging verbosity.")
     return parser
 
 
 def main():
     parser = create_parser()
     args = parser.parse_args()
+    slogging.setup(args.log_level, False)
     return create_and_train_nn_prediction_from_file(**vars(args))
 
 
--- lookout/style/typos/research/nn_prediction.py
+++ lookout/style/typos/research/nn_prediction.py
@@ -1,29 +1,40 @@
-from typing import Iterable, Tuple
-from gensim.models.fasttext import FastText
 import sys
+from typing import Iterable, Sequence, Tuple
+
+from gensim.models.fasttext import FastText
 try:
     import keras
 except ImportError:
-    sys.exit("TF is required for this analyzer")
-from keras.layers import Dense
+    sys.exit("TensorFlow is required for this analyzer")
+from keras.layers import Dense, BatchNormalization, Activation
 from keras.models import Sequential
 import numpy
 import pandas
 
-from lookout.style.typos.utils import collect_embeddings, CORRECT_TOKEN_COLUMN, TYPO_COLUMN
+from lookout.style.typos.utils import extract_embeddings_from_fasttext, \
+    CORRECT_TOKEN_COLUMN, TYPO_COLUMN
 
 
-def get_features(fasttext: FastText, typos: Iterable[str]) -> numpy.ndarray:
-    return numpy.concatenate((collect_embeddings(fasttext, typos),
+def get_features(fasttext: FastText, typos: Sequence[str]) -> numpy.ndarray:
+    return numpy.concatenate((extract_embeddings_from_fasttext(fasttext, typos),
                              numpy.ones((len(typos), 1))), axis=1)
 
 
 def get_target(fasttext: FastText, identifiers: Iterable[str]) -> numpy.ndarray:
-    return collect_embeddings(fasttext, identifiers)
+    return extract_embeddings_from_fasttext(fasttext, identifiers)
 
 
 def generator(features: numpy.ndarray, target: numpy.ndarray, batch_size: numpy.ndarray
               ) -> Tuple[numpy.ndarray, numpy.ndarray]:
+    """
+    Pumps the data for keras.Model.fit_generator()
+
+    :param features: Inputs.
+    :param target: Labels.
+    :param batch_size: Batch size.
+    :return: Another batch for fit_generator().
+    """
+
     while True:
         indices = numpy.random.randint(features.shape[0], size=batch_size)
         batch_features = features[indices]
@@ -31,10 +42,22 @@ def generator(features: numpy.ndarray, target: numpy.ndarray, batch_size: numpy.
         yield batch_features, batch_target
 
 
-def create_model(input_len: int, output_len: int) -> keras.models.Sequential:
+def create_model(num_neurons: int, input_len: int, output_len: int) -> keras.models.Sequential:
+    """
+    Builds the fully-connected NN.
+
+    :param num_neurons: Number of neurons in each hidden layer.
+    :param input_len: Input size.
+    :param output_len: Output size.
+    :return: Built model.
+    """
     model = Sequential()
-    model.add(Dense(units=256, activation="relu", input_dim=input_len))
-    model.add(Dense(units=256, activation="relu"))
+    model.add(Dense(units=num_neurons, input_dim=input_len))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Dense(units=num_neurons))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
     model.add(Dense(units=output_len))
     return model
 
@@ -46,68 +69,78 @@ def train_model(model: keras.models.Sequential, features: numpy.ndarray, target:
                   loss="cosine_proximity")
     model.fit_generator(generator(features, target, batch_size=batch_size),
                         steps_per_epoch=len(features) // batch_size, epochs=num_epochs)
-
     if save_model_file is not None:
         model.save(save_model_file)
     return model
 
 
-def create_and_train_nn_prediction(fasttext: FastText, data: pandas.DataFrame,
-                                   save_model_file: str = None,
-                                   batch_size: int = 64, lr: float = 0.1, decay: float = 1e-7,
-                                   num_epochs: int = 100) -> keras.models.Sequential:
+DEFAULT_NUM_NEURONS = 256
+DEFAULT_BATCH_SIZE = 64
+DEFAULT_LR = 0.1
+DEFAULT_DECAY = 0.9
+DEFAULT_NUM_EPOCHS = 10
+
+
+def create_and_train_nn_prediction(
+        fasttext: FastText, data: pandas.DataFrame, saved_model_file: str,
+        num_neurons: int = DEFAULT_NUM_NEURONS, batch_size: int = DEFAULT_BATCH_SIZE,
+        lr: float = DEFAULT_LR, decay: float = DEFAULT_DECAY, num_epochs: int = DEFAULT_NUM_EPOCHS
+        ) -> keras.models.Sequential:
     """
-    Train nn model for correction embedding prediction.
+    Train NN model for correction embedding prediction.
+
     :param fasttext: gensim.models.Fasttext model.
     :param data: DataFrame containing columns [CORRECT_TOKEN_COLUMN, TYPO_COLUMN].
-    :param save_model_file: Path to file to dump trained nn model.
+    :param saved_model_file: Path to file to dump trained NN model.
+    :param num_neurons: Number of neurons in each hidden layer.
     :param batch_size: Batch size for training.
     :param lr: Learning rate.
-    :param decay: Decay.
-    :param num_epochs: Number of epochs.
-    :return: Trained model.
+    :param decay: Learning rate exponential decay per epoch.
+    :param num_epochs: Number of passes over the train dataset.
+    :return: Trained Keras model.
     """
     typo_vecs = get_features(fasttext, data[TYPO_COLUMN])
     correction_vecs = get_target(fasttext, data[CORRECT_TOKEN_COLUMN])
-
-    model = create_model(typo_vecs.shape[1], correction_vecs.shape[1])
-
-    train_model(model, typo_vecs, correction_vecs, save_model_file, batch_size, lr, decay,
-                num_epochs)
-    if save_model_file is not None:
-        model.save(save_model_file)
+    model = create_model(num_neurons, typo_vecs.shape[1], correction_vecs.shape[1])
+    train_model(
+        model, typo_vecs, correction_vecs, saved_model_file, batch_size, lr, decay, num_epochs)
+    if saved_model_file is not None:
+        model.save(saved_model_file)
     return model
 
 
 def get_predictions(fasttext: FastText, model: keras.models.Sequential, typos: Iterable[str]
                     ) -> numpy.ndarray:
     """
-    Get predicted correction embeddings for tokens from typos
-    :param fasttext: gensim.models.Fasttext model.
-    :param model: Trained nn model
-    :param typos: Iterable with tokens to check
-    :return: Array of predicted correction embeddings
+    Get predicted correction embeddings for tokens from typos.
+
+    :param fasttext: gensim.models.FastText model.
+    :param model: Trained NN model.
+    :param typos: Iterable with tokens to check.
+    :return: Array of predicted correction embeddings.
     """
     return model.predict(get_features(fasttext, typos))
 
 
-def create_and_train_nn_prediction_from_file(fasttext_file: str, data_file: str,
-                                             save_model_file: str = None, batch_size: int = 64,
-                                             lr: float = 0.1, decay: float = 1e-7,
-                                             num_epochs: int = 100) -> keras.models.Sequential:
+def create_and_train_nn_prediction_from_file(
+        fasttext: str, data: str, dump: str = None, num_neurons: int = DEFAULT_NUM_NEURONS,
+        batch_size: int = DEFAULT_BATCH_SIZE, lr: float = DEFAULT_LR, decay: float = DEFAULT_DECAY,
+        num_epochs: int = DEFAULT_NUM_EPOCHS) -> keras.models.Sequential:
     """
-    Train nn model for correction embedding prediction from files.
-    :param fasttext_file: Path to binary dump of fasttext model.
-    :param data_file: Path to csv dump of pandas.DataFrame containing columns
-                      [CORRECT_TOKEN_COLUMN, TYPO_COLUMN].
-    :param save_model_file: Path to file to dump trained nn model.
+    Train NN model for correction embedding prediction from files.
+
+    :param fasttext: Path to the binary dump of a FastText model.
+    :param data: Path to a CSV dump of pandas.DataFrame containing columns \
+                 [CORRECT_TOKEN_COLUMN, TYPO_COLUMN].
+    :param dump: Path to the file where to dump the trained NN model.
+    :param num_neurons: Number of neurons in each hidden layer.
     :param batch_size: Batch size for training.
     :param lr: Learning rate.
-    :param decay: Decay.
-    :param num_epochs: Number of epochs.
-    :return: Trained model.
+    :param decay: Learning rate exponential decay per epoch.
+    :param num_epochs: Number of training passes over the dataset.
+    :return: Trained Keras model.
     """
-    fasttext = FastText.load_fasttext_format(fasttext_file)
-    data = pandas.read_csv(data_file, index_col=0).infer_objects()
-    return create_and_train_nn_prediction(fasttext, data, save_model_file, batch_size, lr, decay,
-                                          num_epochs)
+    fasttext_model = FastText.load_fasttext_format(fasttext)
+    df = pandas.read_csv(data, index_col=0).infer_objects()
+    return create_and_train_nn_prediction(
+        fasttext_model, df, dump, num_neurons, batch_size, lr, decay, num_epochs)
