f445c4915105cfd913cc6bfc56bc3662dfa74c17
Konstantin Slavnov <kslavnov@gmail.com>
2018-10-24 13:41:55 +0300

    Move __version__ to style submodule

--- lookout/__init__.py
+++ lookout/__init__.py
@@ -2,4 +2,3 @@
 # DO NOT CHANGE OR ADD ANYTHING HERE
 import pkg_resources
 pkg_resources.declare_namespace(__name__)
-__version__ = 0, 0, 3
--- lookout/style/__init__.py
+++ lookout/style/__init__.py
@@ -1,2 +1,3 @@
 """Supported machine learning based analyzers for lookout."""
+__version__ = 0, 0, 3
 __meta__ = True
