4d8d35e48127bd89096f5882c7a5832c7c33dfeb
Hugo Mougard <hugo@sourced.tech>
2018-11-01 16:51:27 +0100

    Refactor visualizer to use TypeScript and React hooks
    Signed-off-by: Hugo Mougard <hugo@sourced.tech>

---
+++ lookout/style/format/visualizer/src/SampleToken.tsx
@@ -0,0 +1,29 @@
+// @flow
+import * as React from "react";
+import "./css/Token.css";
+
+export interface IProps {
+  correct: boolean,
+  enabled: boolean,
+  highlightCallback: (index: number) => void,
+  highlighted: boolean,
+  index: number,
+  neighbour: boolean,
+  value: string,
+}
+
+const SampleToken = (props: IProps) => {
+  return (
+    <span className={"feature" +
+                     (props.highlighted ? " highlighted" : "") +
+                     (props.neighbour ? " neighbour" : "") +
+                     (props.enabled && props.correct ? " correct" : "") +
+                     (props.enabled && !props.correct ? " wrong" : "") +
+                     (!props.enabled ? " disabled" : "")}
+          onClick={(_e) => props.highlightCallback(props.index)}>
+      {props.value}
+    </span>
+  );
+}
+
+export default SampleToken;
