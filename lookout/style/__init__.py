5ebde72d14bd212784a96f88a062c7d1ce849fdb
Vadim Markovtsev <vadim@sourced.tech>
2018-12-19 15:05:30 +0100

    Fix pypi deployment by tag
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- lookout/style/__init__.py
+++ lookout/style/__init__.py
@@ -1,3 +1,3 @@
-"""Supported machine learning based analyzers for lookout."""
-__version__ = 0, 1, 0
+"""Machine learning-based analyzers for Lookout - code style."""
+__version__ = "0.1.0rc2"
 __meta__ = True
