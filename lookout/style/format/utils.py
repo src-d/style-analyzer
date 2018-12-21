4548cabf1ecd2d913e4f0736d9ac2a6609b374a1
Waren Long <waren@sourced.tech>
2018-12-21 18:04:25 +0100

    Remove files filtering methods
    Signed-off-by: Waren Long <waren@sourced.tech>

--- lookout/style/format/utils.py
+++ lookout/style/format/utils.py
@@ -8,8 +8,6 @@ from lookout.core.api.service_analyzer_pb2 import Comment
 from lookout.core.api.service_data_pb2 import File
 from tqdm import tqdm
 
-from lookout.style.format.files_filtering import filter_filepaths
-
 
 class FakeDataStub:
     """Fake data source."""
@@ -30,46 +28,6 @@ class FakeDataStub:
         :return: sequence of files.
         """
         return self.files
-
-
-def prepare_files(filenames: Iterable[str], client: BblfshClient,
-                  language: str) -> Iterable[File]:
-    """
-    Prepare the given folder for analysis by extracting UASTs and creating the gRPC wrappers.
-
-    :param filenames: List of paths to files to analyze.
-    :param client: Babelfish client. Babelfish server should be started accordingly.
-    :param language: Language to consider. Will discard the other languages.
-    :return: Iterator of File-s with content, uast, path and language set.
-    """
-    files = []
-    for file in tqdm(filter_filepaths(list(filenames))):
-        try:
-            res = client.parse(file)
-        except NonUTF8ContentException:
-            # skip files that can't be parsed because of UTF-8 decoding errors.
-            continue
-        if res.status == 0 and res.language.lower() == language.lower():
-            uast = res.uast
-            path = file
-            with open(file) as f:
-                content = f.read().encode("utf-8")
-            files.append(File(content=content, uast=uast, path=path,
-                              language=res.language.lower()))
-    return files
-
-
-def prepare_data_stub(input_pattern: str, client: BblfshClient, language: str):
-    """
-    Prepare the given folder for analysis and mimic DataStub from core.
-
-    :param input_pattern: Path to folder with source code -  should be in a format compatible with
-                          glob (ends with **/*  and surrounded by quotes. Ex: `path/**/*`).
-    :param client: Babelfish client. Babelfish server should be started accordingly.
-    :param language: Language to consider. Will discard the other languages.
-    :return: Iterator of File-s with content, uast, path and language set.
-    """
-    return FakeDataStub(files=prepare_files(input_pattern, client, language))
 
 
 def merge_dicts(*dicts: Mapping) -> dict:
