b81234c15d0f1dabc16302cb9c21f66f58132e41
Irina <irenekhismatullina@gmail.com>
2019-02-06 14:35:53 +0300

    Refactor pick_subset.py in typos
    Signed-off-by: Irina <irenekhismatullina@gmail.com>

--- lookout/style/typos/research/pick_subset.py
+++ lookout/style/typos/research/pick_subset.py
@@ -4,12 +4,16 @@ import pandas
 from lookout.style.typos.research.dev_utils import rand_bool
 
 
+def pick_subset_of_df(data: pandas.DataFrame, portion: float, weight_column: str) -> pandas.DataFrame:
+    pick_indices = [rand_bool(portion) for _ in range(len(data))]
+    if weight_column in data.columns:
+        weights = numpy.array(weight_column)
+        average = numpy.sum(weights * 1.0 / len(weights))
+        pick_indices = [rand_bool(1.0 * portion * weight / average) for weight in weights]
+    return data[pick_indices]
+
+
 def pick_subset(args):
-    data = pandas.read_pickle(args.input_file)
-    pick_indices = [rand_bool(args.picked_portion) for _ in range(len(data))]
-    if args.weight_column in data.columns:
-        weights = numpy.array(args.weight_column)
-        average = numpy.sum(weights * 1.0 / len(occs))
-        pick_indices = [rand_bool(1.0 * args.picked_portion * weight / average) for weight in weights]
-    data = data[pick_indices]
-    data.to_pickle(args.out_file)
+    data = pandas.read_csv(args.input_file, index_col=0)
+    pick_subset_of_df(data, args.picked_portion, args.weight_column).to_csv(args.out_file)
+
