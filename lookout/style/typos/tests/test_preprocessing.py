1a7916117463bf431085cd2a6d4e93cff2eca755
Irina <irenekhismatullina@gmail.com>
2019-02-20 18:49:19 +0300

    Add test for pick_subset_of_df_rows
    Signed-off-by: Irina <irenekhismatullina@gmail.com>

---
+++ lookout/style/typos/tests/test_preprocessing.py
@@ -0,0 +1,21 @@
+import unittest
+
+import pandas
+
+from lookout.style.typos.preprocessing import pick_subset_of_df_rows
+from lookout.style.typos.utils import Columns
+
+
+class PreprocessingTest(unittest.TestCase):
+    def test_pick_subset_of_df_rows(self):
+        data = pandas.DataFrame([["get value", "get"],
+                                 ["get value", "value"],
+                                 ["gut", "gut"],
+                                 ["put tok", "put"],
+                                 ["put tok", "tok"],
+                                 ["put", "put"]], columns=[Columns.Split, Columns.Token])
+        self.assertTupleEqual(pick_subset_of_df_rows(data, size=4).shape, (4, 2))
+
+
+if __name__ == "__main__":
+    unittest.main()
