2ba53c0b70eebc47abbed2c4ab2a674edf229c8b
Irina <irenekhismatullina@gmail.com>
2019-02-28 15:02:02 +0300

    Replace 'B' with 'MB' in tqdm wrapper for files loading
    Signed-off-by: Irina <irenekhismatullina@gmail.com>

--- lookout/style/typos/datasets_preparation.py
+++ lookout/style/typos/datasets_preparation.py
@@ -34,7 +34,7 @@ class _DownloadProgressBar(tqdm):
 
 
 def _download_url(url: str, output_path: str) -> None:
-    with _DownloadProgressBar(unit="B", unit_scale=True,
+    with _DownloadProgressBar(unit="MB", unit_scale=True,
                               miniters=1, desc=url.split("/")[-1]) as t:
         urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
 
