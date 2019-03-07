a3525b59acd9dd04be76e1c44e0d8a68b72724a0
Irina <irenekhismatullina@gmail.com>
2019-03-07 19:24:54 +0300

    Apply suggestion from @zurk
    Signed-off-by: Irina <irenekhismatullina@gmail.com>

--- lookout/style/typos/preparation.py
+++ lookout/style/typos/preparation.py
@@ -98,9 +98,9 @@ def prepare_data(params: Optional[Mapping[str, Any]] = None) -> pandas.DataFrame
         raw_data_path = os.path.join(params["data_dir"], params["raw_data_filename"])
         _download_url(params["dataset_url"], raw_data_path)
 
-    try:
+    if raw_data_path.endswith(".xz"):
         data = pandas.read_csv(raw_data_path, index_col=0)
-    except UnicodeDecodeError:
+    else:
         data = pandas.read_csv(raw_data_path, compression="xz", index_col=0)
     if params["frequency_column"] not in data.columns:
         data[Columns.Frequency] = 1
