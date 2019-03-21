45902f49d40d387eb8356727806c73a634b4c12d
Vadim Markovtsev <vadim@sourced.tech>
2019-03-21 22:44:54 +0100

    Change the required js driver version
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- lookout/style/format/langs/javascript/__init__.py
+++ lookout/style/format/langs/javascript/__init__.py
@@ -1,2 +1,2 @@
-"""Javascript specific resources and methods for the format analyzer."""
-__version__ = "==1.3.0"
+"""Javascript-specific resources and methods for the format analyzer."""
+__version__ = ">=2.7.0,<3.0"
